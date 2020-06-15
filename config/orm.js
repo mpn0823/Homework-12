0c222e42a41e78824f7140dc723a24f926d8d510
Michael P Nance <michael.p.nance@gmail.com>
2020-06-14 22:25:46 -0400

    Added awaits to queries in orm.js

--- config/orm.js
+++ config/orm.js
@@ -6,14 +6,14 @@ const util = require(`util`);
 const query = util.promisify(connection.query.bind(connection));
 
 const orm = {
-    selectAll: () =>
-        query(`select * from burgers`),
+    selectAll: async() =>
+        await query(`select * from burgers`),
 
-    insertOne: (str, bool) =>
-        query(`insert into burgers(burger_name, devoured) values(?, ?)`, [str, bool]),
+    insertOne: async(str, bool) =>
+        await query(`insert into burgers(burger_name, devoured) values(?, ?)`, [str, bool]),
 
-    updateOne: (str, bool) =>
-        query(`update burgers set devoured = ? where burger_name = ?`, [bool, str])
+    updateOne: async(str, bool) =>
+        await query(`update burgers set devoured = ? where burger_name = ?`, [bool, str])
 }
 
 module.exports = orm;
