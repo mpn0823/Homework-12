8218154f5de60822d841a862898226c995a385a7
Michael P Nance <michael.p.nance@gmail.com>
2020-06-14 22:03:41 -0400

    Updated connection.js to work with JAWSDB Heroku addon

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,7 @@
 
 const mysql = require(`mysql`);
 
-const connection = mysql.createConnection({
-    host: `localhost`,
-    port: 3306,
-    user: `root`,
-    password: `password`,
-    database: `burgers_db`
-});
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(err =>
     err ? console.log(`error connection: ${err.stack}`) : console.log(`connected as id ${connection.threadId}`));
